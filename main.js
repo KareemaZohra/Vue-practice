6c553efdb4b12db4de65d9d5c1999b1fea914d21
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2021-10-13 01:29:46 +0600

    attribute binding done
    added images src attribute by v-bind

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
     data : function() {
         return{
-            product : "Socks",
-            description : "Comfortable light-weight summer socks"
+            product : 'Socks',
+            description : 'Comfortable light-weight summer socks',
+            image: './assets/christmas-sock.jpg'
         }
     }
 })
