4d7596b3c3e7954b6734fe00bf26726c5b437c9e
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2021-10-14 14:12:57 +0600

    list rendering
    of an array of objects with keys

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ const app = Vue.createApp({
             image: './assets/christmas-sock.jpg',
             url : 'https://www.facebook.com',
             inventory: 8,
-            details : ['50% cotton','30% wool','20% polyester']
+            details : ['50% cotton','30% wool','20% polyester'],
+            variants : [
+                {id : 001, color : 'red'},
+                {id : 002, color : 'green'}
+            ]
+            // variants is an array of objects
         }
     }
 })
