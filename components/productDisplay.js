b617e760265c71cb71e5092bcfbe4dfad5a1d013
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2021-10-26 17:55:00 +0600

    created component
    for product display

---
+++ components/productDisplay.js
@@ -0,0 +1,88 @@
+app.component('product-display',{
+    template : 
+    /*html*/
+    `<div class="row">
+    <div class="product-img col-md-6">
+        <img :src="image">
+    </div>
+    <div class="col-md-6">
+        <h1>{{ title }}</h1>
+        <h4 class="inventory">
+            <span v-if="inventory>10">In-stock</span>
+            <span v-else-if="inventory<=10 && inventory>0">Almost stock-out</span>
+            <span v-else>Stock-out</span>
+        </h4>
+        <a :href="url">
+            <p>{{ description }}</p>
+        </a>
+        <ul>
+            <li v-for="detail in details">{{detail}}</li>
+        </ul>
+        
+        <!-- <div class="color-variant" v-for="variant in variants" :key="variant.id"
+         @mouseover="updateImage(variant.image)" :style="{'background-color': variant.color}"></div> -->
+
+         <div class="color-variant" v-for="(variant, index) in variants" :key="variant.id"
+         @mouseover="updatevariant(index)" :style="{'background-color': variant.color}"></div>
+
+        <!-- <button id="add2cart" class="btn btn-primary" v-on:click="addToCart" :disabled="!inStock"
+         :class="{ disabledButton : !inStock }">
+            Add to cart
+        </button> -->
+
+        <button id="add2cart" v-on:click="addToCart" :disabled="!inStock"
+         :class="[ inStock? 'activeBtn' : 'disabledButton' ]">
+            Add to cart
+        </button>
+    </div>
+</div>`,
+
+data() {
+    return{
+        brand : 'Nike',
+        product : 'Socks',
+        description : 'Comfortable light-weight summer socks',
+        //image: './assets/christmas-sock.jpg',
+        url : 'https://www.facebook.com',
+        inventory: 8,
+        details : ['50% cotton','30% wool','20% polyester'],
+        variants : [
+            {id : 001, color : 'red', image: 'assets/christmas-sock.jpg', quantity : 30},
+            {id : 002, color : 'green', image: 'assets/green-sock.jpg', quantity : 0}
+        ],
+        // variants is an array of objects
+        //inStock : false,
+        selectedVariant: 0
+    }
+},
+methods: {
+    addToCart(){
+        this.cart+=1;
+        // this points to the cart key of data section.
+    },
+    updateImage(varImage){
+        this.image=varImage;
+    },
+    updatevariant(index){
+        // here index is loop index; 0,1,2... etc
+        this.selectedVariant=index;
+    }
+},
+computed: {
+    title(){
+        return this.brand+' '+this.product;
+    },
+    image() {
+        return this.variants[this.selectedVariant].image
+    },
+    inStock(){
+        if(this.variants[this.selectedVariant].quantity>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+})
--- main.js
+++ main.js
@@ -1,50 +1,8 @@
 const app = Vue.createApp({
-    data : function() {
+    data() {
         return{
-            brand : 'Nike',
-            product : 'Socks',
-            description : 'Comfortable light-weight summer socks',
-            //image: './assets/christmas-sock.jpg',
-            url : 'https://www.facebook.com',
-            inventory: 8,
-            details : ['50% cotton','30% wool','20% polyester'],
-            variants : [
-                {id : 001, color : 'red', image: 'assets/christmas-sock.jpg', quantity : 30},
-                {id : 002, color : 'green', image: 'assets/green-sock.jpg', quantity : 0}
-            ],
-            // variants is an array of objects
             cart : 0,
-            //inStock : false,
-            selectedVariant: 0
-        }
-    },
-    methods: {
-        addToCart(){
-            this.cart+=1;
-            // this points to the cart key of data section.
-        },
-        updateImage(varImage){
-            this.image=varImage;
-        },
-        updatevariant(index){
-            // here index is loop index; 0,1,2... etc
-            this.selectedVariant=index;
-        }
-    },
-    computed: {
-        title(){
-            return this.brand+' '+this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock(){
-            if(this.variants[this.selectedVariant].quantity>0){
-                return true;
-            }
-            else{
-                return false;
-            }
+            premium : true
         }
     }
 })
